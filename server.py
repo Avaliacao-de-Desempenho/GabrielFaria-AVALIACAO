11ebf60a2876c994480a6f879cfce3cf6e6fa292
gabriel faria <gabrielfariasilva10@hotmail.com>
2025-07-02 14:51:52 -0300

    feat: /docs post method that saves image in local environment for testing reasons added

--- server.py
+++ server.py
@@ -1,8 +1,37 @@
-from fastapi import FastAPI
+import base64
+import os
 
-app = FastAPI()
+from fastapi import FastAPI, File, UploadFile
+
+# Instancia uma aplicação FastAPI
+app = FastAPI(
+    title="API - Analizador de notas fiscais",
+    description="API desenvolvida utilizando FastAPI que integra a API do Gemini para recuperar campos de notas fiscais.",
+    version="0.1.0",
+)
+
+
+# Recupera variáveis relativas à integração com GEMINI do .env
+CHAVE_API_GEMINI = os.environ.get("CHAVE_API_GEMINI")
+ENDPOINT_GEMINI = os.environ.get("ENDPOINT_GEMINI")
 
 
 @app.get("/")
 def get_teste():
     return {"Hello": "World"}
+
+
+@app.post("/docs/")
+async def processar_documento(arquivo: UploadFile = File(...)):
+    """
+    Função destinada para envio de arquivo / processamento via Gemini da mesma
+
+    Parâmetros:
+        arquivo (UploadFile): Imagem da nota fiscal enviada via API.
+    """
+    # Recupera o arquivo na forma de bytes
+    conteudo = await arquivo.read()
+    with open("teste.png", "wb") as imagem:
+        imagem.write(conteudo)
+
+    return {"teste": "foi"}
