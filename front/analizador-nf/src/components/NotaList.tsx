ac4ed1bcc7992f76ba875dd0a92608fbce204b2c
gabriel faria <gabrielfariasilva10@hotmail.com>
2025-07-10 15:53:49 -0300

    feat: animated spin added to get and delete pages, indicating wheter the website is loading or not

--- front/analizador-nf/src/components/NotaList.tsx
+++ front/analizador-nf/src/components/NotaList.tsx
@@ -11,15 +11,19 @@ interface Nota {
 const ListarNotas = () => {
   const [notas, setNotas] = useState<Nota[]>([])
   const [erro, setErro] = useState<string | null>(null)
+  const [carregando, setCarregando] = useState(true)
 
   useEffect(() => {
     // Função que traz os dados da API
     const fetchNotas = async () => {
       try {
+        setCarregando(true)
         const response = await axios.get("https://api-avaliacao-40pbyn1o.uc.gateway.dev/")
         setNotas(response.data.payload)
       } catch (err: any) {
         setErro(err?.message || "Erro ao buscar notas")
+      } finally {
+        setCarregando(false)
       }
     }
 
@@ -30,17 +34,25 @@ const ListarNotas = () => {
   return (
     <div className="p-6">
       <h2 className="text-xl font-bold mb-4">Notas Registradas</h2>
+
       {erro && <div className="text-red-600">{erro}</div>}
-      <ul className="space-y-2">
-        {notas.map((nota) => (
-          <li key={nota.Index} className="bg-white shadow p-4 rounded-lg">
-            <p><strong>ID:</strong> {nota.Index}</p>
-            <p><strong>Valor:</strong> {nota.Valor}</p>
-            <p><strong>CNPJ:</strong> {nota.CNPJ}</p>
-            <p><strong>Data:</strong> {nota.Data}</p>
-          </li>
-        ))}
-      </ul>
+
+      {carregando ? (
+        <div className="flex justify-center items-center h-40">
+          <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-600 border-opacity-50"></div>
+        </div>
+      ) : (
+        <ul className="space-y-2">
+          {notas.map((nota) => (
+            <li key={nota.Index} className="bg-white shadow p-4 rounded-lg">
+              <p><strong>ID:</strong> {nota.Index}</p>
+              <p><strong>Valor:</strong> R$ {nota.Valor.toFixed(2)}</p>
+              <p><strong>CNPJ:</strong> {nota.CNPJ}</p>
+              <p><strong>Data:</strong> {nota.Data}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
